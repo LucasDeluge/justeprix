1db23e05c66f5fe6e7302a944ab779a2ded66876
LucasDeluge <deluge999@gmail.com>
2022-05-16 14:11:34 +0200

    adds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import JustePrix from "./Components/JustePrix"
 
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <JustePrix />
+    <main>
+      <JustePrix />
+    </main>
   );
 }
 
--- src/Components/JustePrix.jsx
+++ src/Components/JustePrix.jsx
@@ -4,13 +4,20 @@ function JustePrix() {
   // Créer un state pour le prix à trouver
   const [prix, setPrix] = useState(Math.round(Math.random() * 100));
   const [message, setMessage] = useState(null);
+  const lot = [
+    "Une cigarette",
+    "Un gode",
+    "Un cahier de dessins",
+    "Une nuit avec Myriam",
+  ];
 
   const handleSubmit = (event) => {
+    const lotChoisi = Math.round(Math.random() * lot.length);
     // Empêche l'envoi du formulaire et le reload de la page
     event.preventDefault();
-    // alert(event.target[0].value + "," + prix);
+    alert(event.target[0].value + "," + prix);
     if (parseInt(event.target[0].value) === prix) {
-      setMessage("gagné");
+      setMessage("Vous avez gagné : " + lot[lotChoisi]);
     } else {
       if (parseInt(event.target[0].value) > prix) {
         setMessage("Plus petit");
@@ -20,13 +27,26 @@ function JustePrix() {
     }
   };
 
+  const handleReload = (restart, event) => {
+    restart.reload();
+    if (parseInt(event.target[0].value) === prix) {
+      restart.style = { display: "block" };
+    }
+  };
+
   return (
     <Fragment>
       <form onSubmit={(event) => handleSubmit(event)}>
-        <input type="text" placeholder="Proposer un prix" />
+        <input type="text" autoFocus placeholder="Proposer un prix" />
         <input type="submit" value="Proposer" />
       </form>
       <div>{message}</div>
+      <form
+        onSubmit={(restart) => handleReload(restart)}
+        style={{ display: "none" }}
+      >
+        <button>Recommencer</button>
+      </form>
     </Fragment>
   );
 }
