4acd7fa30f675f08e5296933655444edb18b68c3
LucasDeluge <deluge999@gmail.com>
2022-05-16 16:04:46 +0200

    adds

--- src/Components/JustePrix.jsx
+++ src/Components/JustePrix.jsx
@@ -1,54 +1,117 @@
-import { Fragment, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
-function JustePrix() {
-  // Créer un state pour le prix à trouver
-  const [prix, setPrix] = useState(Math.round(Math.random() * 100));
-  const [message, setMessage] = useState(null);
-  const lot = [
-    "Une cigarette",
-    "Un gode",
-    "Un cahier de dessins",
-    "Une nuit avec Myriam",
+export default function JustePrix() {
+  const [error, setError] = useState(0);
+  const [status, setStatus] = useState(null);
+  const [priceToFind, setPriceToFind] = useState(
+    Math.round(Math.random() * 100)
+  );
+  const [isDone, setIsDone] = useState(false);
+  const [formRestart, setFormRestart] = useState(null);
+  const inputPrice = useRef(null);
+  const btnSubmit = useRef(null);
+  const gifts = [
+    "cadeau 1",
+    "cadeau 11",
+    "cadeau 21",
+    "cadeau 31",
+    "cadeau 41",
   ];
+  const [randomGift, setRandomGift] = useState(
+    gifts[Math.ceil(Math.random() * gifts.length - 1)]
+  );
 
-  const handleSubmit = (event) => {
-    const lotChoisi = Math.round(Math.random() * lot.length);
-    // Empêche l'envoi du formulaire et le reload de la page
-    event.preventDefault();
-    alert(event.target[0].value + "," + prix);
-    if (parseInt(event.target[0].value) === prix) {
-      setMessage("Vous avez gagné : " + lot[lotChoisi]);
-    } else {
-      if (parseInt(event.target[0].value) > prix) {
-        setMessage("Plus petit");
+  const inputPreparation = () => {
+    inputPrice.current.value = "";
+    inputPrice.current.focus();
+  };
+
+  const init = () => {
+    setError(0);
+    setPriceToFind(Math.round(Math.random() * 100));
+    setRandomGift(gifts[Math.ceil(Math.random() * gifts.length - 1)]);
+    setStatus(null);
+    setIsDone(false);
+    inputPreparation();
+    btnSubmit.current.disabled = false;
+  };
+
+  const handleRestart = (e) => {
+    e.preventDefault();
+    init();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //end game?
+    if (isDone) {
+      btnSubmit.current.disabled = true;
+      if (error >= 7) {
+        setStatus("Vous avez perdu !");
       } else {
-        setMessage("Plus grand");
+        setStatus("Vous avez gagné le lot suivant : " + randomGift);
+        setFormRestart(
+          <form onSubmit={(e) => handleRestart(e)}>
+            <button type="submit" className="button is-secondary">
+              Recommencer
+            </button>
+          </form>
+        );
+      }
+      return false;
+    }
+    //if not end game
+    if (priceToFind === parseInt(inputPrice.current.value)) {
+      btnSubmit.current.disabled = true;
+      setStatus("Vous avez gagné le lot suivant : " + randomGift);
+      setFormRestart(
+        <form onSubmit={(e) => handleRestart(e)}>
+          <button type="submit" className="button is-secondary">
+            Recommencer
+          </button>
+        </form>
+      );
+      setIsDone(true);
+    } else {
+      setError(error + 1);
+      if (parseInt(inputPrice.current.value) > priceToFind) {
+        setStatus("Plus petit!");
+      } else {
+        setStatus("Plus grand!");
+      }
+      if (error === 7) {
+        btnSubmit.current.disabled = true;
+        setFormRestart(
+          <form onSubmit={(e) => handleRestart(e)}>
+            <button type="submit" className="button is-secondary">
+              Recommencer
+            </button>
+          </form>
+        );
+        setIsDone(true);
       }
     }
+    inputPreparation();
   };
 
-  const handleReload = (restart, event) => {
-    restart.reload();
-    if (parseInt(event.target[0].value) === prix) {
-      restart.style = { display: "block" };
-    }
-  };
+  useEffect(() => {
+    inputPreparation();
+  }, []);
 
   return (
-    <Fragment>
-      <form onSubmit={(event) => handleSubmit(event)}>
-        <input type="text" autoFocus placeholder="Proposer un prix" />
-        <input type="submit" value="Proposer" />
+    <>
+      <div>Compteur d'erreur : {error} (vous avez 7 essais)</div>
+      <div>Indication : {status} </div>
+      <form onSubmit={(e) => handleSubmit(e)} className="has-text-centered">
+        <input type="text" ref={inputPrice} className="input is-primary" />
+        <input
+          type="submit"
+          ref={btnSubmit}
+          value="Proposer"
+          className="button is-primary mt-3"
+        />
       </form>
-      <div>{message}</div>
-      <form
-        onSubmit={(restart) => handleReload(restart)}
-        style={{ display: "none" }}
-      >
-        <button>Recommencer</button>
-      </form>
-    </Fragment>
+      {formRestart}
+    </>
   );
 }
-
-export default JustePrix;
