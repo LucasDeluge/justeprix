3bbbf3a27b6add82bd55a427c6b066458dd9f99b
LucasDeluge <deluge999@gmail.com>
2022-05-17 08:44:41 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import JustePrix from "./Components/JustePrix"
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bulma/css/bulma.min.css';
 
 function App() {
   return (
